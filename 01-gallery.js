bc44e5097059e58a22c822e6afd98d4173f3d927
Konstantin7898 <lexus7898@gmail.com>
2022-07-30 08:37:05 +0300

    js-home-7

--- 01-gallery.js
+++ 01-gallery.js
@@ -1,19 +1,30 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryArray = document.querySelector(".gallery");
+console.log(galleryArray);
 const imgEl = galleryItems
   .map(({ preview, original, description }) => {
-    return `<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>`;
+    return `<li><a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a></li>`;
   })
   .join("");
-
+galleryArray.insertAdjacentHTML("beforeend", imgEl);
 console.log(galleryItems);
-console.log(imgEl);
+
+galleryArray.addEventListener("click", onGalleryArrayClick);
+
+function onGalleryArrayClick(e) {
+  if (!e.target.classlist.contains("gallery__image")) {
+    return;
+  }
+
+  const image = e.target.dataset.value;
+  console.log(image);
+  const instance = basicLightbox.create(`
+    <img src="${image}" width="800" height="600">
+`);
+  console.log(instance);
+  instance.show();
+  instance.show(() => console.log("lightbox now visible"));
+}
